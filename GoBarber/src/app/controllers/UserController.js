7f2e2204c052a955c1d5d600425d3bb9c4b3453a
Sadosco <sadoscolima@gmail.com>
2020-02-13 18:57:51 -0200

    atualizar usuario

--- GoBarber/src/app/controllers/UserController.js
+++ GoBarber/src/app/controllers/UserController.js
@@ -15,6 +15,32 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const UserExists = await User.findOne({ where: { email } });
+
+      if (UserExists)
+        return res.status(400).json({ Error: 'User already exists!' });
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ Error: 'Password not match!' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
