e8d4986c21d61b914370a9d5240f2ca52c38a45a
Sadosco <sadoscolima@gmail.com>
2020-02-13 14:03:06 -0200

    criando middlewares de autenticacao

---
+++ GoBarber/src/app/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+import { promisify } from 'util';
+
+export default async (req, res, next) => {
+  const authMiddleware = req.headers.authorization;
+
+  if (!authMiddleware)
+    return res.status(401).json({ Error: 'Token not provided!' });
+
+  const [, token] = authMiddleware.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.id = decoded.id;
+  } catch (err) {
+    return res.status(401).json({ Error: 'Token invalid!' });
+  }
+
+  return next();
+};
