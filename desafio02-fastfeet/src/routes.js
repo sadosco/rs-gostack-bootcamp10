36af822c9a1319fea2d74c88c40a258dadf9341a
Sadosco <sadoscolima@gmail.com>
2020-02-27 13:04:32 -0300

    Modificações e inserção de rotas de criação de usuário

--- desafio02-fastfeet/src/routes.js
+++ desafio02-fastfeet/src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+
+import './database/index';
 
 const routes = new Router();
 
-routes.get('/api', (req, res) => {
-  res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Igor',
+    email: 'sadoscolima@gmail.com',
+    password_hash: '1234',
+  });
+
+  return res.json(user);
 });
+
+routes.post('/user', UserController.store);
 
 export default routes;
