adf04ee8355cad50cb155dde99f83a103c828b20
Sadosco <sadoscolima@gmail.com>
2020-02-27 10:54:28 -0300

    Arrumando identação

--- desafio02-fastfeet/src/routes.js
+++ desafio02-fastfeet/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/api', (req, res) => {
-  res.json({ message: "hello world"});
+  res.json({ message: 'hello world' });
 });
 
 export default routes;
