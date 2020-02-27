869f59ab98954efb95c1caa3ee9283721bbafbc1
Sadosco <sadoscolima@gmail.com>
2020-02-27 18:34:20 -0300

    Criação de rotas de sessão e criação de usuário

--- desafio02-fastfeet/src/routes.js
+++ desafio02-fastfeet/src/routes.js
@@ -1,21 +1,12 @@
 import { Router } from 'express';
-import User from './app/models/User';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 import './database/index';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Igor',
-    email: 'sadoscolima@gmail.com',
-    password_hash: '1234',
-  });
-
-  return res.json(user);
-});
-
 routes.post('/user', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
