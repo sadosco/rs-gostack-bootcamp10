bf112c932dc09d97b265470b959bd465e558f06e
Sadosco <sadoscolima@gmail.com>
2020-03-03 12:49:52 -0300

    Migration para criação da tabela de destinatários

--- desafio02-fastfeet/src/database/migrations/20200228194846-create-recipients.js
+++ desafio02-fastfeet/src/database/migrations/20200228194846-create-recipients.js
@@ -1,6 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -27,6 +33,14 @@ module.exports = {
       },
       cep: {
         type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
         allowNull: false,
       },
     });
