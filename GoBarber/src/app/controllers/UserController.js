a95eb1bdcf1fb1e95ec5fc5332d4256e0d8e5744
Sadosco <sadoscolima@gmail.com>
2020-03-10 14:30:28 -0300

    Validação de dados utilizando o Yup

--- GoBarber/src/app/controllers/UserController.js
+++ GoBarber/src/app/controllers/UserController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ Error: 'Validations fails!' });
+
     const UserExists = await User.findOne({ where: { email: req.body.email } });
 
     if (UserExists) return res.status(400).json({ Error: 'User exists!' });
@@ -17,6 +31,23 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        confirmPassword == password ? field : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ Error: 'Validations fails!' });
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
