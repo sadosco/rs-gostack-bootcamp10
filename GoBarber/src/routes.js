5c02f8ecbeb557fd23697acc5ad6adb9c5604a8a
Sadosco <sadoscolima@gmail.com>
2020-03-10 15:11:41 -0300

    Criação da rota provider

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -7,6 +7,7 @@ import multerConfig from '../src/config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,5 +19,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
