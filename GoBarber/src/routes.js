d563dc2c09c7a4af419c5be78457a50be4d23383
Sadosco Lima <sadoscolima@gmail.com>
2020-03-19 15:59:27 -0300

    Rota de index dos agendamentos dos prestadores de serviços

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -9,6 +9,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,5 +23,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
