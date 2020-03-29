1abfcc08218008925b0b7e4777ffe9354b7932be
Sadosco Lima <sadoscolima@gmail.com>
2020-03-29 01:27:02 -0300

    Pastas de layout do hbs

---
+++ GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "199cc63686f0a3",
+    pass: "c63f3e99cb5413"
+  },
+  default: {
+    from: 'Equipe GoBarber: <noreply@gobarber.com>',
+  },
+}
