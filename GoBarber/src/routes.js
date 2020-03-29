3b19fba37a3ca97c8a7c40193a89fd1ce967ead7
Sadosco Lima <sadoscolima@gmail.com>
2020-03-29 01:26:26 -0300

    Rotas de agendamento listagem e delete

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -23,7 +23,10 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedules', ScheduleController.index);
 
