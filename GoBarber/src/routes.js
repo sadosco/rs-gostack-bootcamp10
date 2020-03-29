7065cecc9d50e5638a70a7ba49b608d0529858d2
Sadosco Lima <sadoscolima@gmail.com>
2020-03-29 03:27:43 -0300

    Nova rota de agendamentos disponíveis

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -11,6 +11,8 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -21,8 +23,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
@@ -32,5 +35,7 @@ routes.get('/schedules', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
