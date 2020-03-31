d86ff0ae84c62b2cb4ba63a0e16e0c4ac0d9dd3a
Sadosco Lima <sadoscolima@gmail.com>
2020-03-31 19:18:39 -0300

    Route to create a new deliveryman

--- FastFeet/src/routes.js
+++ FastFeet/src/routes.js
@@ -5,6 +5,7 @@ import authMiddlewares from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 const routes = new Router();
 
@@ -14,5 +15,8 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddlewares);
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient', RecipientController.update);
+
+routes.get('/deliveryman', DeliverymanController.index);
+routes.post('/deliveryman', DeliverymanController.store);
 
 export default routes;
