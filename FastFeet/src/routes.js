233dfdb0c6e0b5ed1d39e751e91146865d375e02
Sadosco Lima <sadoscolima@gmail.com>
2020-04-01 02:38:05 -0300

    File routes using multer

--- FastFeet/src/routes.js
+++ FastFeet/src/routes.js
@@ -1,18 +1,25 @@
-import { Router } from 'express';
 import './database/index';
+
+import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 import authMiddlewares from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/user', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddlewares);
+
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient', RecipientController.update);
 
@@ -20,5 +27,7 @@ routes.get('/deliveryman', DeliverymanController.index);
 routes.post('/deliveryman', DeliverymanController.store);
 routes.put('/deliveryman/:id', DeliverymanController.update);
 routes.delete('/deliveryman/:id', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.create);
 
 export default routes;
