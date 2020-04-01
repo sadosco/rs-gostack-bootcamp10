7f24bfd39d68e06ed67f47ddda645d7465a634f8
Sadosco Lima <sadoscolima@gmail.com>
2020-04-01 01:09:11 -0300

    Deliveryman crud functions

--- FastFeet/src/app/controllers/DeliverymanController.js
+++ FastFeet/src/app/controllers/DeliverymanController.js
@@ -2,25 +2,56 @@ import Deliveryman from '../models/Deliveryman';
 
 class DeliverymanController {
   async index(req, res) {
-    return res.json({ id: req.id });
+    const deliverymans = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email'],
+    });
+
+    return res.json(deliverymans);
   }
 
   async store(req, res) {
-    const { name, email } = req.body;
-
-    const deliverymanExists = await Deliveryman.findOne({ where: { email } });
+    const deliverymanExists = await Deliveryman.findOne({
+      where: { email: req.body.email },
+    });
 
     if (deliverymanExists)
       return res
         .status(400)
         .json({ Error: 'This deliveryman already exists!' });
 
-    const deliveryman = await Deliveryman.create({
+    const { id, name, email } = await Deliveryman.create(req.body);
+
+    return res.json({
+      id,
       name,
       email,
     });
+  }
 
-    return res.json(deliveryman);
+  async update(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman)
+      return res.status(400).json({ Error: 'Deliveryman not exists!' });
+
+    const { id, name, email } = await deliveryman.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
+
+  async delete(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    if (!deliveryman)
+      return res.status(400).json({ Error: 'Deliveryman not exists! ' });
+
+    await deliveryman.destroy();
+
+    return res.send();
   }
 }
 
