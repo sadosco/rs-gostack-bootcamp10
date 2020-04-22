003cdc680705d62572ba9614b44dbd568a2b55c9
Sadosco <sadoscolima@gmail.com>
2020-04-22 18:58:56 -0300

    project init, routes create and global styles

---
+++ arquitetura-flux/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+      <GlobalStyles />
+    </BrowserRouter>
+  );
+}
+
+export default App;
