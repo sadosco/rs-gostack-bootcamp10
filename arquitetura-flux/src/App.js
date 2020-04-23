e3dbf0db06935d2562a0154fbb48748d25afdeb1
Sadosco <sadoscolima@gmail.com>
2020-04-22 21:15:46 -0300

    creating header

--- arquitetura-flux/src/App.js
+++ arquitetura-flux/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import Routes from './routes';
 import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
