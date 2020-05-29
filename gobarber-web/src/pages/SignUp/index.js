138fa20e8cfb449cdf28d883db87acdae36e33f2
Sadosco <sadoscolima@gmail.com>
2020-05-29 16:31:41 -0300

    Estilização da autenticação

--- gobarber-web/src/pages/SignUp/index.js
+++ gobarber-web/src/pages/SignUp/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
 
 export default SignUp;
